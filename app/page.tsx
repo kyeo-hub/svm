775fbe7a584b82cd2fc0c7624e3afc52460f1a5e
小草爸[爆竹] <oncwnuJS9X27CUG19B-jUgkunmpE@git.weixin.qq.com>
2025-07-31 14:56:28 +0800

    修改标题名

--- app/page.tsx
+++ app/page.tsx
@@ -480,7 +480,8 @@ export default function Home() {
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-bold text-gray-900">特种车辆设备管理系统</h1>
+            <h1 className="text-3xl font-bold text-gray-900">特种车辆设备管理系统--SVM</h1>
+            <h2 className="text-xl font-bold text-gray-900">special vehicles manage</h2>
             <nav className="flex space-x-4">
               <Link 
                 href="/stats" 
